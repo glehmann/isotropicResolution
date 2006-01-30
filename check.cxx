9f30976b06b39878277942678504f35a118db980
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-01-30 17:26:57 +0100

    add 2D tests
    darcs-hash:20060130162657-2fc9d-fa1158825c502ea78423ff37c1e28905dcfbbe05.gz

--- check.cxx
+++ check.cxx
@@ -2,8 +2,9 @@
 #include "itkImageFileWriter.h"
 #include "itkCommand.h"
 #include "itkSimpleFilterWatcher.h"
+#include "itkChangeInformationImageFilter.h"
 
-#include "itkImageFilter.h"
+#include "itkIsotropicResolutionImageFilter.h"
 
 
 int main(int, char * argv[])
@@ -15,18 +16,29 @@ int main(int, char * argv[])
 
   typedef itk::ImageFileReader< IType > ReaderType;
   ReaderType::Pointer reader = ReaderType::New();
-  reader->SetFileName( argv[1] );
+  reader->SetFileName( argv[3] );
 
-  typedef itk::ImageFilter< IType, IType > FilterType;
+  typedef itk::ChangeInformationImageFilter< IType > ChangeInfoType;
+  ChangeInfoType::Pointer changeInfo = ChangeInfoType::New();
+  itk::Vector<double, 2> spacing;
+  spacing[0] = 0.1;
+  spacing[1] = 0.33;
+  changeInfo->SetInput(reader->GetOutput());
+  changeInfo->SetOutputSpacing(spacing);
+  changeInfo->SetChangeSpacing(true);
+
+  typedef itk::IsotropicResolutionImageFilter< IType, IType > FilterType;
   FilterType::Pointer filter = FilterType::New();
-  filter->SetInput( reader->GetOutput() );
+  filter->SetInput( changeInfo->GetOutput() );
+  filter->SetMaximumIncrease( atof(argv[1]) );
+  filter->SetNearestNeighbor( atoi(argv[2]) );
 
   itk::SimpleFilterWatcher watcher(filter, "filter");
 
   typedef itk::ImageFileWriter< IType > WriterType;
   WriterType::Pointer writer = WriterType::New();
   writer->SetInput( filter->GetOutput() );
-  writer->SetFileName( argv[2] );
+  writer->SetFileName( argv[4] );
   writer->Update();
 
   return 0;
