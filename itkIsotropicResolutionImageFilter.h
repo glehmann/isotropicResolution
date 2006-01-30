6f62f3959159d815abe9165a7bd0bbfaf699d8d2
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-01-30 11:04:18 +0100

    import previous work
    darcs-hash:20060130100418-2fc9d-79d63bd0b910644ac64a902c8306004885f571b6.gz

---
+++ itkIsotropicResolutionImageFilter.h
@@ -0,0 +1,146 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkIsotropicResolutionImageFilter.h,v $
+  Language:  C++
+  Date:      $Date: 2005/04/04 14:37:29 $
+  Version:   $Revision: 1.1 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkIsotropicResolutionImageFilter_h
+#define __itkIsotropicResolutionImageFilter_h
+
+#include <vector>
+#include <queue>
+#include "itkImageToImageFilter.h"
+#include "itkImage.h"
+#include "itkNumericTraits.h"
+#include "itkConceptChecking.h"
+#include "itkInterpolateImageFunction.h"
+
+namespace itk
+{
+/**
+ * \class IsotropicResolutionImageFilter
+ * \brief Grow input image to create a new image with isotropic resolution
+ *
+ * IsotropicResolutionImageFilter is a convenient filter to get a image with isotropic
+ * resolution from an image with anisotropic resolution. It increase the size of the
+ * input image to get the same spacing on each axes.
+ * IsotropicResolutionImageFilter is far less powerful than ResampleImageFilter (even
+ * if it use it internally), but is also far much simple to use and to integrate in 
+ * a pipeline. If you need to fine tune the settings to resample your image, you should
+ * prefer ResampleImageFilter.
+ *
+ * You can set a maximum increase of number of pixel on each axe. If the maximum is
+ * reached IsotropicResolutionImageFilter will set the spacing on the axe to be the
+ * maximum authorized, and will decrease the number of pixels on the other axes to 
+ * keep an isotropic resolution on output image.
+ * 
+ * Note that the choice of interpolator function can be important.
+ * This function is set via SetInterpolator().  The default is
+ * itk::LinearInterpolateImageFunction<InputImageType, TInterpolatorPrecisionType>, which
+ * is reasonable for ordinary medical images.  However, some synthetic
+ * images have pixels drawn from a finite prescribed set.  An example
+ * would be a mask indicating the segmentation of a brain into a small
+ * number of tissue types.  For such an image, one does not want to
+ * interpolate between different pixel values, and so
+ * itk::NearestNeighborInterpolateImageFunction< InputImageType,
+ * TInterpolatorPrecisionType > would be a better choice.
+ *
+ * \sa ResampleImageFilter
+ */
+template <class TInputImage, class TOutputImage, class TInterpolatorPrecisionType=double>
+class ITK_EXPORT IsotropicResolutionImageFilter :
+    public ImageToImageFilter< TInputImage, TOutputImage >
+{
+public:
+  /** Extract dimension from input and output image. */
+  itkStaticConstMacro(InputImageDimension, unsigned int,
+                      TInputImage::ImageDimension);
+  itkStaticConstMacro(OutputImageDimension, unsigned int,
+                      TOutputImage::ImageDimension);
+
+  /** Convenient typedefs for simplifying declarations. */
+  typedef TInputImage InputImageType;
+  typedef TOutputImage OutputImageType;
+
+  /** Standard class typedefs. */
+  typedef IsotropicResolutionImageFilter Self;
+  typedef ImageToImageFilter< InputImageType, OutputImageType> Superclass;
+  typedef SmartPointer<Self> Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(IsotropicResolutionImageFilter, ImageToImageFilter);
+
+  /** Image typedef support. */
+  typedef typename InputImageType::PixelType InputPixelType;
+  typedef typename OutputImageType::PixelType OutputPixelType;
+  typedef typename NumericTraits<InputPixelType>::RealType InputRealType;
+  typedef typename InputImageType::OffsetType OffsetType;
+  typedef typename InputImageType::IndexType IndexType;
+
+  typedef typename InputImageType::RegionType InputImageRegionType;
+  typedef typename OutputImageType::RegionType OutputImageRegionType;
+  typedef typename InputImageType::SizeType InputSizeType;
+
+  /** Interpolator typedef. */
+  typedef InterpolateImageFunction<InputImageType, TInterpolatorPrecisionType> InterpolatorType;
+  typedef typename InterpolatorType::Pointer  InterpolatorPointerType;
+
+  /** Set the interpolator function.  The default is
+   * itk::LinearInterpolateImageFunction<InputImageType, TInterpolatorPrecisionType>. Some
+   * other options are itk::NearestNeighborInterpolateImageFunction
+   * (useful for binary masks and other images with a small number of
+   * possible pixel values), and itk::BSplineInterpolateImageFunction
+   * (which provides a higher order of interpolation).  */
+  itkSetObjectMacro( Interpolator, InterpolatorType );
+
+  /** Get a pointer to the interpolator function. */
+  itkGetConstObjectMacro( Interpolator, InterpolatorType );
+
+  /** Set the maximum increase spacing factor. Default is 0, which means no limit. */
+  itkSetMacro(MaximumIncrease, double);
+
+  /** Get the maximum increase factor. */
+  itkGetMacro(MaximumIncrease, double);
+
+protected:
+  IsotropicResolutionImageFilter();
+  virtual ~IsotropicResolutionImageFilter(){}
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  virtual void GenerateInputRequestedRegion() ;
+  
+  virtual void GenerateOutputInformation();
+  
+  void  GenerateData ();
+  
+private:
+  IsotropicResolutionImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+  
+  InterpolatorPointerType m_Interpolator; // Image function for interpolation
+  
+  double m_MaximumIncrease;
+
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkIsotropicResolutionImageFilter.txx"
+#endif
+
+#endif
---
+++ itkIsotropicResolutionImageFilter.txx
@@ -0,0 +1,182 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkIsotropicResolutionImageFilter.txx,v $
+  Language:  C++
+  Date:      $Date: 2005/08/23 15:56:54 $
+  Version:   $Revision: 1.2 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkIsotropicResolutionImageFilter_txx
+#define _itkIsotropicResolutionImageFilter_txx
+
+#include "itkResampleImageFilter.h"
+#include <itkLinearInterpolateImageFunction.h>
+#include "itkProgressAccumulator.h"
+
+namespace itk
+{
+        
+template <class TInputImage, class TOutputImage, class TInterpolatorPrecisionType>
+IsotropicResolutionImageFilter<TInputImage, TOutputImage, TInterpolatorPrecisionType>
+::IsotropicResolutionImageFilter()
+{
+  m_Interpolator = LinearInterpolateImageFunction<InputImageType, TInterpolatorPrecisionType>::New();
+  m_MaximumIncrease = 0;
+}
+
+template <class TInputImage, class TOutputImage, class TInterpolatorPrecisionType>
+void 
+IsotropicResolutionImageFilter<TInputImage, TOutputImage, TInterpolatorPrecisionType>
+::GenerateInputRequestedRegion()
+{
+  // call the superclass's implementation of this method
+  Superclass::GenerateInputRequestedRegion();
+
+  if ( !this->GetInput() )
+    {
+    return;
+    }
+
+  // get pointers to the input and output
+  typename InputImageType::Pointer  inputPtr  = 
+    const_cast< TInputImage *>( this->GetInput() );
+
+  // Request the entire input image
+  InputImageRegionType inputRegion;
+  inputRegion = inputPtr->GetLargestPossibleRegion();
+  inputPtr->SetLargestPossibleRegion(inputRegion);
+  inputPtr->SetRequestedRegion(inputRegion);
+
+  return;
+}
+
+/** 
+ * Inform pipeline of required output region
+ */
+template <class TInputImage, class TOutputImage, class TInterpolatorPrecisionType>
+void 
+IsotropicResolutionImageFilter<TInputImage, TOutputImage, TInterpolatorPrecisionType>
+::GenerateOutputInformation()
+{
+  // call the superclass' implementation of this method
+  Superclass::GenerateOutputInformation();
+
+  // get pointers to the input and output
+  typename OutputImageType::Pointer outputPtr = this->GetOutput();
+  if ( !outputPtr )
+    {
+    return;
+    }
+    
+  typename InputImageType::SpacingType inputSpacing = this->GetInput()->GetSpacing();
+  typename InputImageType::SpacingType isoSpacing;
+  
+  typename InputImageType::SizeType inputSize = this->GetInput()->GetLargestPossibleRegion().GetSize();
+  typename OutputImageType::SizeType outputSize;
+  
+  // find the smallest spacing
+  double smallestSpacing = NumericTraits< double >::max();
+  double biggestSpacing = 0;
+  for( int i=0; i < InputImageDimension; i++)
+    {
+    if ( smallestSpacing > inputSpacing[i] )
+      {
+      smallestSpacing = inputSpacing[i];
+      }
+    if ( biggestSpacing < inputSpacing[i] )
+      {
+      biggestSpacing = inputSpacing[i];
+      }
+    }
+    
+  if ( m_MaximumIncrease != 0 && biggestSpacing / smallestSpacing > m_MaximumIncrease )
+    {
+    isoSpacing.Fill( biggestSpacing / m_MaximumIncrease );
+    }
+  else
+    {
+    // use the smallest spacing on all axes
+    isoSpacing.Fill( smallestSpacing );
+    }
+    
+  // output size
+  for( int i=0; i < InputImageDimension; i++)
+    {
+      outputSize[i] = static_cast<unsigned long>( inputSize[i] * inputSpacing[i] / isoSpacing[i] );
+    }
+  
+  outputPtr->SetLargestPossibleRegion( outputSize );
+  outputPtr->SetSpacing( isoSpacing );
+  outputPtr->SetOrigin( this->GetInput()->GetOrigin() );
+  outputPtr->SetDirection( this->GetInput()->GetDirection() );
+  
+  return;
+}
+
+        
+
+template< class TInputImage, class TOutputImage, class TInterpolatorPrecisionType>
+void
+IsotropicResolutionImageFilter< TInputImage, TOutputImage, TInterpolatorPrecisionType>
+::GenerateData( void )
+{
+  // Allocate the outputs
+  this->AllocateOutputs();
+
+  // input spacing
+  typename InputImageType::SpacingType inputSpacing = this->GetInput()->GetSpacing();
+  
+  // output spacing and size
+  typename InputImageType::SpacingType isoSpacing = this->GetOutput()->GetSpacing();
+  typename OutputImageType::SizeType outputSize = this->GetOutput()->GetLargestPossibleRegion().GetSize();
+  
+  /** set up resample image filter */
+  typename ResampleImageFilter<TInputImage, TOutputImage >::Pointer
+    resample = ResampleImageFilter<TInputImage, TOutputImage, TInterpolatorPrecisionType>::New();
+
+  resample->SetInput( this->GetInput() );
+  resample->ReleaseDataFlagOn();
+  resample->SetOutputSpacing( isoSpacing );
+  resample->SetSize( outputSize );
+  resample->SetInterpolator( m_Interpolator );
+  
+  /** set up the minipipeline */
+  typename ProgressAccumulator::Pointer progress = ProgressAccumulator::New();
+  progress->SetMiniPipelineFilter(this);
+  progress->RegisterInternalFilter(resample, 1.0f);
+  
+  resample->GraftOutput( this->GetOutput() );
+
+  /** execute the minipipeline */
+  resample->Update();
+
+  /** graft the minipipeline output back into this filter's output */
+  this->GraftOutput( resample->GetOutput() );
+}
+
+
+/**
+ * Standard "PrintSelf" method
+ */
+template <class TInputImage, class TOutputImage, class TInterpolatorPrecisionType>
+void
+IsotropicResolutionImageFilter<TInputImage, TOutputImage, TInterpolatorPrecisionType>
+::PrintSelf( std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf( os, indent );
+  os << indent << "Interpolator: " << std::endl;
+  m_Interpolator->Print(os, indent.GetNextIndent());
+  os << indent << "MaximumIncrease: " << m_MaximumIncrease << std::endl;
+}
+
+} // end namespace itk
+
+#endif
